ad6cd70586c599f6c451238a285c00dcd9c946c6
DezireM <deziremaatkabylova@gmail.com>
2024-07-26 17:39:50 +0600

    Initial commit

--- app/src/main/java/com/example/notes2/ui/fragments/note/NoteFragment.kt
+++ app/src/main/java/com/example/notes2/ui/fragments/note/NoteFragment.kt
@@ -1,25 +1,30 @@
 package com.example.notes2.ui.fragments.note
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notes2.App
+import com.example.notes2.Interface.OnClickItem
 import com.example.notes2.R
 import com.example.notes2.data.extension.getBackStackData
 import com.example.notes2.data.models.NoteModel
 import com.example.notes2.databinding.FragmentNoteBinding
 import com.example.notes2.ui.adapters.NoteAdapter
+import com.example.notes2.utils.PreferenceHelper
 
 
-class NoteFragment : Fragment() {
+class NoteFragment : Fragment(), OnClickItem {
 
     private lateinit var binding: FragmentNoteBinding
     private lateinit var noteAdapter: NoteAdapter
-    private var list: ArrayList<NoteModel> = ArrayList()
+    private var isGridLayout: Boolean = false
+    private lateinit var sharedPreferenceHelper: PreferenceHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,15 +36,22 @@ class NoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        noteAdapter = NoteAdapter()
+        sharedPreferenceHelper = PreferenceHelper(requireContext())
+        isGridLayout = sharedPreferenceHelper.getIsGridLayout()
+        noteAdapter = NoteAdapter(onLongClick = this, onClick = this)
         initialize()
         setUpListeners()
         getData()
+        updateLayoutButtonIcon()
     }
 
     private fun initialize() {
         binding.rvNote.apply {
-            layoutManager = LinearLayoutManager(requireContext())
+            layoutManager = if (isGridLayout) {
+                GridLayoutManager(requireContext(), 2)
+            } else {
+                LinearLayoutManager(requireContext())
+            }
             adapter = noteAdapter
         }
     }
@@ -48,13 +60,53 @@ class NoteFragment : Fragment() {
         addBtn.setOnClickListener {
             findNavController().navigate(R.id.action_noteFragment_to_NoteDetailFragment)
         }
+        isGridLayout.setOnClickListener {
+            isGridLayout = true
+            binding.rvNote.layoutManager = GridLayoutManager(requireContext(), 2)
+            sharedPreferenceHelper.setIsGridLayout(isGridLayout)
+            updateLayoutButtonIcon()
+        }
+        linerLayout.setOnClickListener {
+            isGridLayout = false
+            binding.rvNote.layoutManager = LinearLayoutManager(requireContext())
+            sharedPreferenceHelper.setIsGridLayout(isGridLayout)
+            updateLayoutButtonIcon()
+        }
+    }
+
+    private fun updateLayoutButtonIcon() = with(binding) {
+        if (isGridLayout) {
+            gridLayout.visibility = View.GONE
+            linerLayout.visibility = View.VISIBLE
+        } else {
+            gridLayout.visibility = View.VISIBLE
+            linerLayout.visibility = View.GONE
+        }
     }
 
     private fun getData() {
-        App().getInstance()?.noteDao()?.getAll()?.observe(viewLifecycleOwner){
+        App().getInstance()?.noteDao()?.getAll()?.observe(viewLifecycleOwner) {
             noteAdapter.submitList(it)
         }
+    }
 
+    override fun onLongClick(noteModel: NoteModel) {
+        val builder = AlertDialog.Builder(requireContext())
+        with(builder) {
+            setTitle("Вы точно хотите удалить эту заметку?")
+            setPositiveButton("Да") { dialog, which ->
+                App().getInstance()?.noteDao()?.deleteNote(noteModel)
+            }
+            setNegativeButton("Нет") { dialog, which ->
+                dialog.cancel()
+            }
+            show()
+        }
+        builder.create()
+    }
 
+    override fun onClick(noteModel: NoteModel) {
+        val action = NoteFragmentDirections.actionNoteFragmentToNoteDetailFragment(noteModel.id)
+        findNavController().navigate(action)
     }
 }
